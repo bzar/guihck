b277bb3512d1a94fd8c579b6782d53ce73b565a6
Teemu Erkkola <teemu.erkkola@iki.fi>
2014-05-10 13:38:34 +0300

    use quoted list in functions test

--- test/scm/functions.scm
+++ test/scm/functions.scm
@@ -1,8 +1,8 @@
 (define color-button
   (composite rectangle '(width 50 height 50)
-    (mouse-area (list 'onMouseDown '(set-clear-color (get-parent-property 'color))
-                      'width 50
-                      'height 50))))
+    (mouse-area '(onMouseDown (set-clear-color (get-parent-property 'color))
+                  width 50
+                  height 50))))
       
 (create-elements
   (row '(x 100 y 100)
